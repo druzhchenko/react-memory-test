2f791e6d5e88fd3012d5f02ebebb451e7e9f0598
sergey <sergey.druzhchenko@gmail.com>
2020-10-06 13:22:10 +0300

    memory test

--- src/App.js
+++ src/App.js
@@ -1,26 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+const COUNT_OF_INPUTS = 2000;
+
+const btn = {
+  padding: '10px 20px',
+  fontWeight: 600,
+}
+
+const active = {
+  ...btn,
+  border: '2px solid black',
+  outside: 'none',
+  borderRadius: 4,
+  color: 'green',
+}
+
+export default function App() {
+  const [ view, setView ] = React.useState( 'home' );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav>
+        <Button isActive={view === 'home'} view="home" onClick={setView} >Home</Button>
+        <Button isActive={view === 'about'} view="about" onClick={setView} >About</Button>
+        <Button isActive={view === 'users'} view="users" onClick={setView} >Users</Button>
+      </nav>
+      <div>
+        { view === 'home' && <Home/> }
+        { view === 'about' && <About/> }
+        { view === 'users' && <Users/> }
+      </div>
     </div>
   );
 }
 
-export default App;
+function Button({ isActive, view, children, onClick }) {
+  return <button
+      style={ isActive? active : btn }
+      onClick={ () => onClick(view) }
+  >
+    {children}
+  </button> ;
+}
+
+function Form() {
+  const [ values, setValue ] = React.useState(new Array(COUNT_OF_INPUTS).fill( '' ));
+
+  const changeValue = ( key, value ) => {
+    setValue( values.map( ( v, k ) => k === key ? value : v  ) );
+  }
+
+  return  <form>
+    { values.map( ( value, key )  => {
+      return <input
+        size={30}
+        key={`key-${key}`}
+        value={ value }
+        name={`name-${key}`}
+        onChange={e=>changeValue( key, e.currentTarget.value )}
+        placeholder="type here and click inactive button"
+    />
+    }) }
+  </form>
+}
+
+function Home() {
+  return <div>
+    <h2>Home</h2>
+    <Form />
+  </div> ;
+}
+
+function About() {
+  return <div>
+    <h2>About</h2>
+    <Form />
+  </div> ;
+}
+
+function Users() {
+  return <div>
+    <h2>Users</h2>
+    <Form />
+  </div>;
+}
